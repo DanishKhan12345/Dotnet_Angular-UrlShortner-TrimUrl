7ae2c7dba584908a879b7ab13adde5cad2d8cecc
Danish Khan <danishayaan@gmail.com>
2025-08-17 19:33:06 +0530

    Initial Commit : Added Project Structure

---
+++ Models/Url.cs
@@ -0,0 +1,10 @@
+﻿namespace TrimUrlApi.Models
+{
+    public class Url
+    {
+        public int Id { get; set; }
+        public required string LongUrl { get; set; }
+        public required string UrlCode { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
