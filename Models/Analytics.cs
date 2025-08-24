8ddc76e4182862a576b968dc278e4e70eb5da4e6
Danish Khan <danishayaan@gmail.com>
2025-08-24 17:57:45 +0530

    1. Added Table Models

---
+++ Models/Analytics.cs
@@ -0,0 +1,15 @@
+﻿namespace TrimUrlApi.Models
+{
+    public class Analytics
+    {
+        public int Id { get; set; }
+        public DateTime ClickedTime { get; set; }
+        public required string UrlCode { get; set; }
+
+        /// Foreign key to the Url table
+        public int UrlId { get; set; }
+
+        /// Navigation property to the Url
+        public Url? Url { get; set; }
+    }
+}
--- Models/Url.cs
+++ Models/Url.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public required string LongUrl { get; set; }
         public required string UrlCode { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public required DateTime CreatedAt { get; set; }
+
+        public IReadOnlyList<Analytics> Analytics { get; set; } = null!;
     }
 }
+// This code defines a Url class with properties for Id, LongUrl, UrlCode, and CreatedAt.
