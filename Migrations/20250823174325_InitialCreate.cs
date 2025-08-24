15e55600b974a4811ae67426cc1841d244a7b38c
Danish Khan <danishayaan@gmail.com>
2025-08-24 17:58:41 +0530

    Added Migrations and EFCore for table creation

---
+++ Migrations/20250823174325_InitialCreate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TrimUrlApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    LongUrl = table.Column<string>(type: "text", nullable: false),
+                    UrlCode = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Analytics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ClickedTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UrlCode = table.Column<string>(type: "text", nullable: false),
+                    UrlId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Analytics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Analytics_Urls_UrlId",
+                        column: x => x.UrlId,
+                        principalTable: "Urls",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Analytics_UrlId",
+                table: "Analytics",
+                column: "UrlId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Urls_UrlCode",
+                table: "Urls",
+                column: "UrlCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Analytics");
+
+            migrationBuilder.DropTable(
+                name: "Urls");
+        }
+    }
+}
---
+++ Persistence/Context/TrimUrlDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrimUrlApi.Models;
+
+namespace TrimUrlApi.Persistence.Context
+{
+    public class TrimUrlDbContext(DbContextOptions<TrimUrlDbContext> options) : DbContext(options)
+    {
+        public DbSet<Url> Urls { get; init; }
+        public DbSet<Analytics> Analytics { get; init; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure Url entity
+            modelBuilder.Entity<Url>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.LongUrl).IsRequired();
+                entity.Property(u => u.UrlCode).IsRequired();
+                entity.Property(u => u.CreatedAt).HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+            
+            modelBuilder.Entity<Url>().HasIndex(u => u.UrlCode).IsUnique();
+            
+            modelBuilder.Entity<Url>()
+                .HasMany(a => a.Analytics)
+                .WithOne(a => a.Url)
+                .HasForeignKey(a => a.UrlId);
+
+            // Configure Analytics entity
+            modelBuilder.Entity<Analytics>(entity =>
+            {
+                entity.HasKey(a => a.Id);
+                entity.Property(a => a.ClickedTime).HasDefaultValueSql("CURRENT_TIMESTAMP");
+                entity.Property(a => a.UrlCode).IsRequired();
+                entity.HasOne(a => a.Url)
+                      .WithMany(u => u.Analytics)
+                      .HasForeignKey(a => a.UrlId);
+            });
+        }
+    }
+}
